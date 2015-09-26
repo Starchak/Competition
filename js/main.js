3bfe7dd8b2b4437a37eed6c1b40a50af0c56719c
Maksim Maksimenko <imaksmaksimenko@gmail.com>
2015-09-26 15:08:12 +0300

    Done with Opan and Close menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,55 @@
 $(document).ready(function() {
 
-  var openMenu = function() {
+  var menuButton = $('.menuB');
+  var menuImage = $('.menuI');
+  var menu = $('.menu');
+  
+  var menuClick = 0;
+  
+  var Start = function() {
+  }
+  
+  var OpenMenu = function() {
+    menu.animate({'top': '+=100vh'}, 'slow');
+    
     
   }
   
+  var CloseMenu = function() {
+    menu.animate({'top': '-=100vh'}, 'slow');
+  }
+  
+  menuButton.click(function(){
+    if(menuClick % 2 == 0){
+      OpenMenu();
+      menuClick++;
+    }else{
+      CloseMenu();
+      menuClick++;
+    }
+  });
+  
+  menuImage.click(function(){
+    if(menuClick % 2 == 0){
+      OpenMenu();
+      menuClick++;
+    }else{
+      CloseMenu();
+      menuClick++;
+    }
+  });
+    
+  Start();
+  
 });
+
+
+
+
+
+
+
+
+
+
+
